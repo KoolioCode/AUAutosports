417748d3fd783f4b9a5dc10286eedfd6a164dbfb
kynanfox <powellwilliamsjonathan@gmail.com>
2026-04-11 00:05:31 -0500

    hmmm

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+
+// define variables and set to empty values
+$fnameErr = $lnameErr = $emailErr = "";
+$fname = $lname = $email = $message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["fname"])) {
+    $fnameErr = "First name is required";
+  } else {
+    $fname = test_input($_POST["fname"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+  
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+  
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+
+}
+
+  ?>
