91acc0b496251163d58167cfe7beb88e736d4365
kynanfox <powellwilliamsjonathan@gmail.com>
2026-04-11 00:07:18 -0500

    lets see if its js or the htaccess

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 /* JS Document for AU Autosports website */
 
-if (window.location.href.endsWith(".html")) {
-  window.location.href = window.location.href.replace(".html", "");
-}
+
 
 /* disable animations until page is fully loaded to prevent eye strain */
 const html = document.querySelector('html');
