2eae549d989a4b9e2f9d75db4ed1a1b097d27222
kynanfox <powellwilliamsjonathan@gmail.com>
2026-04-15 22:23:32 -0500

    OK IT WORKS

--- js/script.js
+++ js/script.js
@@ -252,7 +252,7 @@ const landingScroll = () => {
 
 
   landingButton.addEventListener('click', () => {
-    Aos.refresh();
+  
     slowScrollTo(document.getElementById('scrollthing'), 2000,96);
 
   });
