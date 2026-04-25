1e2326140a9bbfb21323cb7dbe4d09568a3de805
kynanfox <powellwilliamsjonathan@gmail.com>
2026-04-25 17:39:43 -0500

    dsadsasdadas

--- js/script.js
+++ js/script.js
@@ -261,8 +261,45 @@ landingScroll();
 
 
 
+const counters = document.querySelectorAll(".about-us-counter span");
+const container = document.querySelectorAll(".about-us-counter");
 
+let activated = false;
 
+window.addEventListener(scroll, () => {
+
+  if( pageYOffset>container.offsetTop - container.offsetheight - 200 && activated === false){
+     
+    counters.forEach(counter => {
+
+      counters.innerText = 0;
+
+      let count = 0;
+
+      function updateCount(){
+        const target = parseInt(counter.dataset.count);
+
+        if(count < target){
+          count++;
+          counter.innerText = count;
+          setTimeout(updateCount,10);
+        } else{
+          counter.innerText = target;
+
+        }
+      }
+      updateCount();
+      activated=true;
+    });
+  } else if ( pageYOffset < container.offsetTop - container.offsetHeight - 500 || pageYOffset === 0 && activated === true){
+    counters.forEach(counter => {
+      counter.innerText = 0;
+    });
+
+    activated = false;
+  }
+  
+});
 
 
 
