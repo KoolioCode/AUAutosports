6320039dd15a845176d1a6d6bfbe5f39f6d8355c
ProgWithLog <120297920+KoolioCode@users.noreply.github.com>
2026-05-22 05:41:54 -0500

    Removing broken href replacer thingy

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 /* JS Document for AU Autosports website */
 
-
-  if (window.location.href.endsWith(".html")) {
-    window.location.href = window.location.href.replace(".html", "");
-  }
-
-  if (window.location.href.endsWith("index.html")) {
-    window.location.href = window.location.href.replace("index.html", "");
-  }
-
-
 /* disable animations until page is fully loaded to prevent eye strain */
 const html = document.querySelector('html');
 window.addEventListener("load", () => {
